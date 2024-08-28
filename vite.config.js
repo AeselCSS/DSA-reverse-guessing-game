662c67c5af11cc48f54d4d0df14f8bfdf2c0aae5
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-08-29 01:24:38 +0200

    deployment

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "/DSA-reverse-guessing-game/",
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]'
+            }
+        }
+    }
 });
